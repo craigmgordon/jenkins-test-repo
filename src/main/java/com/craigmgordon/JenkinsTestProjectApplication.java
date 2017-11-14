dfa615098e0444f134a8a0ff353bc284e54729ec
Craig Gordon <craigmgordon@gmail.com>
2017-11-14 21:06:05 +0000

    initial commit

---
+++ src/main/java/com/craigmgordon/JenkinsTestProjectApplication.java
@@ -0,0 +1,12 @@
+package com.craigmgordon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsTestProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsTestProjectApplication.class, args);
+	}
+}
